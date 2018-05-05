a7a25ed720254f52357fa988643ab1f5b7952433
Luis Enrique Silva Martínez <silmar.dll@gmail.com>
2018-05-04 20:13:14 -0500

    parsear cadenas con 2 spares

--- lib/frame_splitter.rb
+++ lib/frame_splitter.rb
@@ -6,6 +6,17 @@ class FrameSplitter
   end
 
   def frames
+    if game[3] == '/' &&  game[15] == '/'
+      return game[0, 2].scan(/../) + [game[2, 3]] + game[4, 10].scan(/../) + [game[14,3]] + game[16..-1].scan(/../)
+    end
+
+    if game[1] == '/' &&  game[17] == '/'
+      return [game[0,3]] + game[2, 14].scan(/../) + [game[16, 3]] + game[18..-1].scan(/../)
+    end
+
+    if game[15] == '/' &&  game[17] == '/'
+      return game[0, 14].scan(/../) + [game[14, 3], game[16, 3]] + game[18..-1].scan(/../)
+    end
     index = game.index('/')
     if index
       return game[0,index - 1].scan(/../) + [game[index - 1, 3]] + game[(index + 1)..-1].scan(/../)
--- spec/frame_splitter_spec.rb
+++ spec/frame_splitter_spec.rb
@@ -111,4 +111,73 @@ describe FrameSplitter do
     end
   end
 
+  context "when the input is '251132745123451/3/45'" do
+    let(:input) { '251132745123451/3/45' }
+
+    it "split frames correctly" do
+      expected = ["25", "11", "32", "74", "51", "23", "45", "1/3", "3/4", "45"]
+      splitter = FrameSplitter.new(input)
+      expect(splitter.frames).to eq expected
+    end
+  end
+
+  context "when the input is '251132745123451/4/45'" do
+    let(:input) { '251132745123451/4/45' }
+
+    it "split frames correctly" do
+      expected = ["25", "11", "32", "74", "51", "23", "45", "1/4", "4/4", "45"]
+      splitter = FrameSplitter.new(input)
+      expect(splitter.frames).to eq expected
+    end
+  end
+
+  context "when the input is '251132745123451/3/44'" do
+    let(:input) { '251132745123451/3/44' }
+
+    it "split frames correctly" do
+      expected = ["25", "11", "32", "74", "51", "23", "45", "1/3", "3/4", "44"]
+      splitter = FrameSplitter.new(input)
+      expect(splitter.frames).to eq expected
+    end
+  end
+
+  context "when the input is '2/113274512345113/44'" do
+    let(:input) { '2/113274512345113/44' }
+
+    it "split frames correctly" do
+      expected = ["2/1", "11", "32", "74", "51", "23", "45", "11", "3/4", "44"]
+      splitter = FrameSplitter.new(input)
+      expect(splitter.frames).to eq expected
+    end
+  end
+
+  context "when the input is '2/213274512345113/44'" do
+    let(:input) { '2/213274512345113/44' }
+
+    it "split frames correctly" do
+      expected = ["2/2", "21", "32", "74", "51", "23", "45", "11", "3/4", "44"]
+      splitter = FrameSplitter.new(input)
+      expect(splitter.frames).to eq expected
+    end
+  end
+
+  context "when the input is '2/313274512345113/44'" do
+    let(:input) { '2/313274512345113/44' }
+
+    it "split frames correctly" do
+      expected = ["2/3", "31", "32", "74", "51", "23", "45", "11", "3/4", "44"]
+      splitter = FrameSplitter.new(input)
+      expect(splitter.frames).to eq expected
+    end
+  end
+
+  context "when the input is '231/23745123451/1344'" do
+    let(:input) { '231/23745123451/1344' }
+
+    it "split frames correctly" do
+      expected = ["23", "1/2", "23", "74", "51", "23", "45", "1/1", "13", "44"]
+      splitter = FrameSplitter.new(input)
+      expect(splitter.frames).to eq expected
+    end
+  end
 end
